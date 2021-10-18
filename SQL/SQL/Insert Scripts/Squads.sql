ca327e496d8e831b1c0a13e36256d0d2d87c1af3
hitesh05 <hitesh.goel05@gmail.com>
2021-10-18 13:45:27 +0530

    added fixtures

---
+++ SQL/SQL/Insert Scripts/Squads.sql
@@ -0,0 +1,11 @@
+----- INSERTING THE TEAMS
+INSERT INTO Squads VALUES (1,'India');
+INSERT INTO Squads VALUES (2,'Australia');
+INSERT INTO Squads VALUES (3,'England');
+INSERT INTO Squads VALUES (4,'New Zealand');
+INSERT INTO Squads VALUES (5,'Pakistan');
+INSERT INTO Squads VALUES (6,'Sri Lanka');
+INSERT INTO Squads VALUES (7,'South Africa');
+INSERT INTO Squads VALUES (8,'Bangladesh');
+INSERT INTO Squads VALUES (9,'West Indies');
+INSERT INTO Squads VALUES (10,'Afghanistan');
