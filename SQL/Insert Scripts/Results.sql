ebf7c26ff51ed1d5e21259bd15d6a1ac15807e07
hitesh05 <hitesh.goel05@gmail.com>
2021-10-18 17:18:28 +0530

    added results table

---
+++ SQL/Insert Scripts/Results.sql
@@ -0,0 +1,96 @@
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (1,'311/8','207/10','England','104 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (2,'105','108/3','West Indies','7 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (3,'136/10','137/0','New Zealand','10 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (4,'207/10','209/3','Australia','7 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (5,'330/6','309/8','Bangladesh','21 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (6,'348/8','334/9','Pakistan','14 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (7,'201/10','152/10','Sri Lanka','34 runs (D\L)');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (8,'227/9','230/4','India','6 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (9,'244/10','248/8','New Zealand','2 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (10,'288/10','273/9','Australia','15 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (11,NULL,NULL,NULL,NULL);
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (12,'386/6','280/10','England','106 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (13,'172/10','173/3','New Zealand','7 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (14,'352/5','316/10','India','36 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (15,NULL,NULL,NULL,NULL);
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (16,NULL,NULL,NULL,NULL);
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (17,'307/10','266/10','AUstralia','41 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (18, NULL, NULL,NULL,NULL);
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (19,'212/10','213/2','England','8 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (10,'334/7','247','Australia','87 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (21,'125/10','131/1','South Africa','9 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (22,'336/5','212/6 (40 overs)','India', '89 runs (D/L)');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (23,'321/8','322/3','Bangladesh','7 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (24,'397/6','247/8','England','150 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (25, '241/6','245/6','New Zealand','4 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (26,'381/6','333/8','Australia','48 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (27, '232/9','212/10','Sri Lanka','20 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (28, '224/8','213/10','India','11 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (29,'291/8','286/10','New Zealand','5 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (30,'308/7','259/9','Pakistan','49 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (31, '262/7','200/10','Bangladesh','62 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (32,'285/7','221/10','Australia','64 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (33,'237/6','241/4','Pakistan','4 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (34,'268/7','143/10','India','125 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (35,'203/10','206/1','South Africa','9 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (36, '227/9','230/7','Pakistan','3 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (37, '243/9','157/10','Australia','86 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (38, '337/7','306/5','England','31 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (39,'338/6','315/9','Sri Lanka','23 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (40,'314/9','286/10','India','28 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (41,'305/8','186/10','England','119 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (42,'311/6','288/10','West Indies','23 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (43,'315/9','221/10','Pakistan','94 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (44,'264/7','265/3','India','7 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (45, '325/6','315/10','South Africa','10 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (46,'239/8','221/10','New Zealand','18 runs');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (47,'223/10','226/2','England','8 wickets');
+INSERT INTO Results (`Match ID`, `Team 1 Score`, `Team 2 Score`, `Winner`, `Win Margin`)
+VALUES (48, '241/8','241/10','England','England won on boundary count');
